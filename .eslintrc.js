a81ec023955fb17a0ea09ac2888053d5e09feceb
Karem Shabana <km425360@gmail.com>
2025-01-27 01:48:58 +0200

    Add react-native ESLint plugin and update configuration for unused styles

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
   extends: ['expo', 'prettier'],
-  plugins: ['prettier'],
+  plugins: ['prettier', 'react-native'],
   rules: {
     'prettier/prettier': [
       'error',
@@ -9,6 +9,7 @@ module.exports = {
         endOfLine: 'auto',
       },
     ],
+    'react-native/no-unused-styles': 'error',
   },
   ignorePatterns: ['/dist/*'],
 };
