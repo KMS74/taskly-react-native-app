70cec38d3c843ec45e196f6bb413faa9ef678738
Karem Shabana <km425360@gmail.com>
2025-03-05 14:43:45 +0200

    feat: implement shopping list functionality with item addition

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,58 @@
+import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TextInput, View } from 'react-native';
 import ShoppingListItem from '../components/ShoppingListItem';
 import { theme } from '../theme';
 
+type ShoppingListItemType = {
+  id: string;
+  name: string;
+};
+
+const initialList: ShoppingListItemType[] = [
+  {
+    id: '1',
+    name: 'Coffee',
+  },
+  {
+    id: '2',
+    name: 'Milk',
+  },
+  {
+    id: '3',
+    name: 'Tea',
+  },
+];
+
 export default function App() {
+  const [shoppingList, setShoppingList] =
+    useState<ShoppingListItemType[]>(initialList);
+
+  const [value, setValue] = useState('');
+
+  const handleAddItem = () => {
+    if (value) {
+      setShoppingList((prev) => [
+        ...prev,
+        { id: new Date().toTimeString(), name: value },
+      ]);
+      setValue('');
+    }
+  };
+
   return (
     <View style={styles.container}>
-      <ShoppingListItem name="Coffee" />
-      <ShoppingListItem name="Milk" isCompleted />
-      <ShoppingListItem name="Bread" isCompleted />
-      <ShoppingListItem name="Eggs" />
-      <ShoppingListItem name="Bananas" />
+      <TextInput
+        placeholder="Add an item"
+        value={value}
+        onChangeText={setValue}
+        returnKeyType="done"
+        onSubmitEditing={handleAddItem}
+      />
+
+      {shoppingList.map((item) => (
+        <ShoppingListItem key={item.id} name={item.name} />
+      ))}
 
       <StatusBar style="auto" />
     </View>
